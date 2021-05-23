cc06f3d0dea82af7e6ebce56cde02ed0ef6c1656
mesyamaureen <mesyamaureen@gmail.com>
2021-05-23 23:56:56 +0200

    Funktionen mit benötigten Parametern in JobanzeigeController erstellen

--- collab-ia01/collab-asp/Controllers/JobanzeigeController.vb
+++ collab-ia01/collab-asp/Controllers/JobanzeigeController.vb
@@ -8,5 +8,37 @@ Namespace Controllers
         Function Index() As ActionResult
             Return View()
         End Function
+
+        'ladenJobanzeigeEinzeln() - für Bewerbung.html?
+        Function LadenJobanzeigeEinzeln(pintJobID As Integer) As Jobanzeige
+            Dim intJobID As Integer
+            Dim strTitel As String
+            Dim strBeschreibung As String
+
+
+        End Function
+
+        'anzeigenJobanzeige()
+        Function AnzeigenJobanzeige(pintJobID As Integer) As ActionResult
+
+        End Function
+
+        'loeschen()
+        Function Loeschen(pintJobID As Integer) As ActionResult
+
+        End Function
+
+        'anzeigenFormular()
+        Function AnzeigenFormular() As ActionResult
+
+        End Function
+
+        'speichern()
+        Function Speichern() As ActionResult
+
+        End Function
+
+        'abbrechen()
+
     End Class
 End Namespace
