14050976a1197b30009970903f15d1f2f8eee12b
mesyamaureen <mesyamaureen@gmail.com>
2021-05-23 11:57:59 +0200

    Controller-Klassen erstellen und ASP als Projektstart

---
+++ collab-ia01/collab-asp/Controllers/InfluencerController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class InfluencerController
+        Inherits Controller
+
+        ' GET: Influencer
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
---
+++ collab-ia01/collab-asp/Controllers/JobanzeigeController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class JobanzeigeController
+        Inherits Controller
+
+        ' GET: Jobanzeige
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
