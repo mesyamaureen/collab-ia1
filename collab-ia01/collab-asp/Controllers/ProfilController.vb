240f32810544b788182b0f4392f1b38405e0a938
n-h-o-c-h <hochtritt@strato.de>
2021-05-24 18:49:22 +0200

    Profil und AlleProfileController erstellt

---
+++ collab-ia01/collab-asp/Controllers/ProfilController.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class ProfilController
+        Inherits Controller
+
+
+
+        ' GET: Profil
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        Function LadenProfil() As ActionResult
+
+        End Function
+
+
+        Function AnzeigenProfil(pintInId As Integer) As Influencer
+
+        End Function
+
+        Function abbrechen() As ActionResult
+
+
+        End Function
+
+        Function speichernBewertung() As ActionResult
+
+        End Function
+
+        Function AnzeigenChat() As ActionResult
+
+        End Function
+
+
+        Function SpeichernNachricht() As ActionResult
+
+        End Function
+
+
+    End Class
+End Namespace
